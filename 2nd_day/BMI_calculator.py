e95f4fed07ab5f507da12056234b3846a8f0715b
Mercedesz-Mollusca <ludanyimercedesz@gmail.com>
2025-07-02 09:04:58 +0200

    Initial commit for Python_course

---
+++ 2nd_day/BMI_calculator.py
@@ -0,0 +1,22 @@
+height = 1.65
+weight = 84
+
+bmi = weight / (height  ** 2)
+print(bmi)
+# create integer
+print(int(bmi))
+# rounds the number
+print(round(bmi))
+
+print(round(bmi, 2))
+
+score = 0
+score += 1
+print(score)
+# f-strings
+score = 0
+height = 1.8
+is_winning = True
+
+print(f"Your score is = {score}, your height is {height}. You are winning {is_winning}")
+
